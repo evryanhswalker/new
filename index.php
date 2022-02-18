108e090341dae117437d9191178cff5d116e2cf4
evryanhswalker <99967962+evryanhswalker@users.noreply.github.com>
2022-02-18 12:27:30 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Landing-Page</title>
+
+    <link rel="stylesheet" href="css/index.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <section class="first-section">
+        <div class="first-section-bg-1">
+
+        </div>
+
+        <img src="images/logo.png" alt="">
+
+
+        <h1>
+            Norton Antivirus 2022 Update
+        </h1>
+        <p>Due to recent reports of <span>Malware</span> and <span>Viruses</span>, Norton has released an Antivirus update.</p>
+        <p>
+            Computers without a Antivirus, could be exposed to viruses and slower performance.</p>
+        <p class="bottom">
+            Clean and improve the performance of your Computer NOW. No computer restart is required!</p>
+
+
+
+        <a href="tel:+1917-719-5277">+1-917-719-5277</a>
+        <div class="blank">
+
+        </div>
+        <a href="tel:+9177195277">Click Here To Enable Now</a>
+
+
+        <div class="first-section-img">
+            <img src="images/main-image.png" alt="">
+        </div>
+
+
+
+    </section>
+
+    <div class="second-section">
+        <div class="second-section-child">
+
+        </div>
+    </div>
+    <footer class="footer-section">
+        <div class="footer-section-child">
+            <p>© Your Privacy Guardian - All Rights Reserved.</p>
+            <p>This website is privately owned and is neither operated by, nor affiliated with Norton, LLC.</p>
+            <p> This site contains affiliate links to products. We may receive a commission for purchases made through these links.</p>
+        </div>
+    </footer>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SN7T144CP3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SN7T144CP3');
+</script>
+
+</body>
+
+</html>
